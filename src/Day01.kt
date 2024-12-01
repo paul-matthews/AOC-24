c96196b5e7c2d0de2d82699e2b90eb48c627899d
Paul Matthews <paul.matthews.86@gmail.com>
2024-12-01 07:23:03 +0000

    Day01 p1 writing before refactoring
    * Completed first Day01 puzzle
    * Code is inefficient and needs refactoring

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,29 @@
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+        input.map {
+            val (left, right) = it.split("""\s+""".toRegex())
+            leftList.add(left.toInt())
+            rightList.add(right.toInt())
+        }
+        leftList.sort()
+        rightList.sort()
+        return leftList.zip(rightList).sumOf { (left, right) ->
+            abs(left - right)
+        }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
