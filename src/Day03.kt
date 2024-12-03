96ec139c600751a6dd39b3b0fa3942e24b53e7c7
Paul Matthews <paul.matthews.86@gmail.com>
2024-12-03 09:36:01 +0000

    Day03 p2 Simple answer
    * Needs refactoring to improve readability
    * Potentially some redundant code in this

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,4 +1,7 @@
 fun main() {
+    val matchInstructionRegex = """((mul|do|don't)\(([0-9,]{0,7})\))""".toRegex()
+    fun MatchResult.getInstruction(): String = groupValues[2]
+
     val multiplierRegex = """(mul\((\d{1,3}),(\d{1,3})\))""".toRegex()
     fun MatchResult.getMultipliers(): Pair<Int, Int> {
         return Pair(groupValues[2].toInt(),groupValues[3].toInt())
@@ -13,11 +16,27 @@ fun main() {
         }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var isMatching = true
+        var total = 0
+        input.map {line ->
+            matchInstructionRegex
+                .findAll(line)
+                .forEach {instruction ->
+                    when(instruction.getInstruction()) {
+                        "don't" -> isMatching = false
+                        "do" -> isMatching = true
+                        "mul" -> if (isMatching) total += part1(listOf(instruction.value))
+                    }
+                }
+        }
+        return total
     }
 
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 161)
+
+    val testInput2 = readInput("Day03_test2")
+    check(part2(testInput2) == 48)
 
     val input = readInput("Day03")
     part1(input).println()
