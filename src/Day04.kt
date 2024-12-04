5763c48e6a850dc58de9601241c696c3c15907a7
Paul Matthews <paul.matthews.86@gmail.com>
2024-12-04 09:31:18 +0000

    Not currently working
    * Over matching number of entries

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,8 @@
 typealias Position = Pair<Int, Int>
 typealias LetterMap = Map<Int, Map<Int, String>>
 
+fun Position.adjust(f: Int, s: Int) = Position(first + f, second + s)
+
 fun main() {
     val xmasRegex = """XMAS""".toRegex()
     val samxRegex = """SAMX""".toRegex()
@@ -39,6 +41,14 @@ fun main() {
         )
     }
 
+    fun getMas(input: LetterMap, position: Position): List<String> {
+        val upwardsMas = listOf(position.adjust(-1, -1), position, position.adjust(1, 1))
+        val downwardsMas = listOf(position.adjust(1, 1), position, position.adjust(-1, -1))
+        val upValue = getStringFromInput(input, upwardsMas)
+        val downValue = getStringFromInput(input, downwardsMas)
+        return listOf(upValue, downValue, upValue.reversed(), downValue.reversed())
+    }
+
     fun part1(input: List<String>): Int {
         val inputMap = input.toLetterMap()
         var count = 0
@@ -57,15 +67,39 @@ fun main() {
         return count
     }
 
-    fun part2(input: List<String>): Int = input.size
+    fun part2(input: List<String>): Int {
+        val inputMap = input.toLetterMap()
+        var count = 0
+        input.mapIndexed() {lineNo, line ->
+            line.indices.map {i ->
+                val current = line[i].toString()
+                if (current == "A") {
+                    val mas = getMas(inputMap, Position(lineNo, i))
+                    val position = Position(lineNo, i)
+                    val prevCount = count
+                    count += if (getMas(inputMap, position).count { it == "MAS" } == 2) 1 else 0
+                    if (prevCount != count)
+                        println("[$position] Count: $count, mas: $mas")
+                        println(line)
+                }
+            }
+        }
+        count += input.sumOf {
+            xmasRegex.findAll(it).count() +
+                    samxRegex.findAll(it).count()
+        }
+        return count
+    }
 
 
     // Or read a large test input from the `src/Day04_test.txt` file:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 18)
+    val p2 = part2(testInput)
+    println(p2)
+    check(p2 == 9)
     // Read the input from the `src/Day04.txt` file.
     val input = readInput("Day04")
     part1(input).println()
-    return
     part2(input).println()
 }
