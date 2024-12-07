71c07d3377bec33f3bf601bfdf094c736482129a
Paul Matthews <paul.matthews.86@gmail.com>
2024-12-07 08:57:07 +0000

    Day07 p2 Quickest change to current solution
    * Still needs a clean up, redundant map and sum calls
    * Also could return false much quicker as no subtraction or division

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,12 +1,14 @@
 fun main() {
-    fun makesTotal(total: Long, values: List<Long>): Boolean {
+    fun makesTotal(total: Long, values: List<Long>, part2: Boolean = false): Boolean {
         if (values.isEmpty()) return false
         if (values.size == 1) return (values.first() == total)
         val first = values.first()
         val second = values.second()
         val otherItems = values.drop(2)
-        listOf(first + second, first * second).map {
-            if (makesTotal(total, listOf(it) + otherItems)) return true
+        var operations = listOf(first + second, first * second)
+        if (part2) { operations = operations + listOf("$first$second".toLong()) }
+        operations.map {
+            if (makesTotal(total, listOf(it) + otherItems, part2)) return true
         }
         return false
     }
@@ -22,19 +24,26 @@ fun main() {
         }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return input.map {line ->
+            val (total, partsStr) = line.split(": ")
+            return@map if (makesTotal(total.toLong(), partsStr.splitOnWhitespace().map { it.toLong() }, part2 = true)) {
+                total.toLong()
+            } else {
+                0
+            }
+        }.sum()
     }
 
     measureExecutionTime {
         // Or read a large test input from the `src/Day07_test.txt` file:
         val testInput = readInput("Day07_test")
         check(part1(testInput) == 3749.toLong())
+        check(part2(testInput) == 11387.toLong())
 
         // Read the input from the `src/Day07.txt` file.
         val input = readInput("Day07")
         part1(input).println()
-        return@measureExecutionTime
         part2(input).println()
     }
 }
