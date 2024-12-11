8a8518dbe54d983b49ea09028ce4665405437408
Paul Matthews <paul.matthews.86@gmail.com>
2024-12-11 19:10:29 +0000

    Day09 p2 Taking it in a different direction to p1
    * Debugging the approach used for p1 was getting too much, so simplified
      the data structures and took the time hit of refactoring
    * Still lots more to do to make this readable code but it works

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,6 +1,20 @@
 
 typealias FileInstance = Pair</* ID */Int, /* Count */Int>
 
+sealed class FileInfo() {
+    abstract val size: Int
+    data class File(val id: Int, override val size: Int): FileInfo() {
+        override fun getReprOnDisk() = List(size) { id }
+        override fun toString(): String = "F($size) #$id"
+    }
+    data class Space(override val size: Int): FileInfo() {
+        override fun getReprOnDisk() = List(size) { 0 }
+        override fun toString(): String = "S($size)"
+    }
+
+    abstract fun getReprOnDisk(): List<Int>
+}
+
 fun main() {
     fun <T> ArrayDeque<T>.applyIfNotEmpty(action: (ArrayDeque<T>) -> Unit) {
         if (this.isNotEmpty()) {
@@ -40,58 +54,51 @@ fun main() {
         return null
     }
 
+    fun <T> ArrayDeque<T>.replaceLastIfTrue(replaceWith: (T) -> T, compare: (T) -> Boolean): T? {
+        (size - 1 downTo 0).forEach { i ->
+            val item = this[i]
+            if(compare(item)) {
+                set(i, replaceWith(item))
+                return item
+            }
+        }
+        return null
+    }
+
+
     fun part2(input: Map<Int, Int>): Long {
-        val files = mutableListOf<Int>()
-        val space = mutableListOf<Int>()
-        val filesStack = ArrayDeque<FileInstance>()
+        val filesInfo = ArrayDeque<FileInfo>()
+        var fileId = 0
         input.forEach() {(pos, value) ->
             if (pos.isEven()) {
-                files.add(value)
-                filesStack.addLast(Pair(files.size - 1, value))
+                filesInfo.addLast(FileInfo.File(fileId++, value))
             } else {
-                space.add(value)
+                filesInfo.addLast(FileInfo.Space(value))
             }
         }
         val diskMap = mutableListOf<Int>()
-        files.withIndex().zip(space).forEach { (indexedFile, spacesCount) ->
-            if (filesStack.isEmpty()) {
-                return@forEach
-            }
-
-            val (_, fileCount) = indexedFile
-            println("[${fileCount}]>> $filesStack")
-            val (fileId, _) = filesStack.removeFirst()
-            repeat(fileCount) { diskMap.add(fileId) }
-            // Get the biggest file that can fit in the space
-            var spaceTarget = spacesCount
-            var remainingSpaces = 0
-            while (spaceTarget > 0) {
-                if (filesStack.isEmpty()) {
-                    remainingSpaces += spaceTarget
-                    break
-                }
-                val spaceFoundFile = filesStack.popLastIfTrue {(id, fileSize) ->
-                    return@popLastIfTrue fileSize <= spaceTarget
-                }
-
-                if (spaceFoundFile != null) {
-                    repeat(spaceFoundFile.second) { diskMap.add(spaceFoundFile.first) }
-                    remainingSpaces = spaceTarget - spaceFoundFile.second
-                    if (remainingSpaces > 0) {
-                        spaceTarget = remainingSpaces
-                        remainingSpaces = 0
-                        continue
+        while (filesInfo.isNotEmpty()) {
+            when (val current = filesInfo.removeFirst()) {
+                is FileInfo.File -> {
+                    diskMap.addAll(current.getReprOnDisk())}
+                is FileInfo.Space -> {
+                    for (biggestSpace in (current.size downTo  0)) {
+                        if (filesInfo.replaceLastIfTrue({
+                            FileInfo.Space(it.size)
+                        }) { (it is FileInfo.File) && it.size <= biggestSpace }?.let { found ->
+                                diskMap.addAll(found.getReprOnDisk())
+                                if (found.size < current.size) {
+                                    filesInfo.addFirst(FileInfo.Space(current.size - found.size))
+                                }
+                                true
+                            } == true) { break }
+                        if (biggestSpace == 0) {
+                            diskMap.addAll(current.getReprOnDisk())
+                        }
                     }
-                    break
                 }
-                spaceTarget--
-                remainingSpaces++
-            }
-            if (remainingSpaces > 0) {
-                repeat(remainingSpaces) {  diskMap.add(0) }
             }
         }
-        println("> $diskMap")
         return diskMap.mapIndexed() { i, value -> i * value.toLong() }.sum()
     }
 
@@ -100,9 +107,8 @@ fun main() {
     check(part1(testInput) == 1928.toLong())
     check(part2(testInput) == 2858.toLong())
 
-    return
     // Read the input from the `src/Day09.txt` file.
     val input = readInput("Day09").first().toNumbers()
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
